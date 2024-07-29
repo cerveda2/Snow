78c10c6122ad199fe85436ece26c224c7ae7eded
David Cervenka <dadacervenka@gmail.com>
2024-07-29 12:04:48 +0200

    App theme

--- app/src/main/java/cz/dcervenka/snow/ui/theme/Color.kt
+++ app/src/main/java/cz/dcervenka/snow/ui/theme/Color.kt
@@ -2,10 +2,22 @@ package cz.dcervenka.snow.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
+/*val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Pink40 = Color(0xFF7D5260)*/
+
+val GreenPrimary = Color(0xFF4CAF50)
+val GreenPrimaryDark = Color(0xFF388E3C)
+val YellowAccent = Color(0xFFFFEB3B)
+val BackgroundWhite = Color(0xFFFFFFFF)
+val TextPrimary = Color(0xFF212121)
+val TextSecondary = Color(0xFF757575)
+val DividerGray = Color(0xFFBDBDBD)
+val SnowInfoBlue = Color(0xFFBBDEFB)
+val TracksOpenGreen = Color(0xFFC8E6C9)
+val ErrorRed = Color(0xFFF44336)
+val SuccessGreen = Color(0xFF8BC34A)
--- app/src/main/java/cz/dcervenka/snow/ui/theme/Theme.kt
+++ app/src/main/java/cz/dcervenka/snow/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package cz.dcervenka.snow.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -9,18 +8,45 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = GreenPrimary,
+    primaryContainer = GreenPrimaryDark,
+    secondary = YellowAccent,
+    background = Color.Black,
+    surface = Color.Black,
+    onPrimary = Color.Black,
+    onSecondary = Color.Black,
+    onBackground = Color.White,
+    onSurface = Color.White,
+    error = ErrorRed,
+    onError = Color.Black,
+
+    secondaryContainer = SnowInfoBlue,
+    tertiary = TracksOpenGreen,
+    tertiaryContainer = SuccessGreen,
+    outline = DividerGray
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = GreenPrimary,
+    primaryContainer = GreenPrimaryDark,
+    secondary = YellowAccent,
+    background = BackgroundWhite,
+    surface = BackgroundWhite,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = TextPrimary,
+    onSurface = TextPrimary,
+    error = ErrorRed,
+    onError = Color.White,
+
+    secondaryContainer = SnowInfoBlue,
+    tertiary = TracksOpenGreen,
+    tertiaryContainer = SuccessGreen,
+    outline = DividerGray
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
