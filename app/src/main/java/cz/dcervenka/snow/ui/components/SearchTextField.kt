d02e9fe1e3b577ba28178bf86ba629ef8a6b256b
David Cervenka <dadacervenka@gmail.com>
2024-08-01 11:45:26 +0200

    Search text field icons logic

--- app/src/main/java/cz/dcervenka/snow/ui/components/SearchTextField.kt
+++ app/src/main/java/cz/dcervenka/snow/ui/components/SearchTextField.kt
@@ -1,9 +1,11 @@
 package cz.dcervenka.snow.ui.components
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.filled.Search
@@ -14,7 +16,14 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusManager
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -27,34 +36,49 @@ fun SearchTextField(
     onTextChange: (TextFieldValue) -> Unit,
     modifier: Modifier = Modifier,
 ) {
+    var isFocused by remember { mutableStateOf(false) }
+    val focusManager: FocusManager = LocalFocusManager.current
+
     OutlinedTextField(
         value = textState,
         onValueChange = onTextChange,
         modifier = modifier
             .fillMaxWidth()
-            .padding(16.dp),
+            .padding(16.dp)
+            .onFocusChanged { focusState ->
+                isFocused = focusState.isFocused
+            },
         placeholder = { Text(text = "Najít středisko") },
         leadingIcon = {
             Icon(
                 imageVector = Icons.Default.Search,
-                //tint = MaterialTheme.colorScheme.secondaryContainer,
                 contentDescription = null
             )
         },
         trailingIcon = {
-            IconButton(onClick = {
-                onFavoriteToggled()
-            }) {
+            if (isFocused) {
                 Icon(
-                    imageVector = if (favoriteToggled) Icons.Filled.Favorite
-                    else Icons.Filled.FavoriteBorder,
-                    //tint = MaterialTheme.colorScheme.secondaryContainer,
-                    contentDescription = null
+                    imageVector = Icons.Default.Clear,
+                    contentDescription = "Clear",
+                    modifier = Modifier.clickable {
+                        onTextChange(TextFieldValue(""))
+                        focusManager.clearFocus()
+                    }
                 )
+            } else {
+                IconButton(onClick = {
+                    onFavoriteToggled()
+                }) {
+                    Icon(
+                        imageVector = if (favoriteToggled) Icons.Filled.Favorite
+                        else Icons.Filled.FavoriteBorder,
+                        contentDescription = null
+                    )
+                }
             }
         },
         colors = OutlinedTextFieldDefaults.colors(
-            focusedBorderColor = MaterialTheme.colorScheme.secondaryContainer
+            focusedBorderColor = MaterialTheme.colorScheme.primary
         ),
         shape = CircleShape
     )
